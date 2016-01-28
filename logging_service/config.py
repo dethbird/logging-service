f23109e243f2bec03356ba9b5dacf31c12943a85
Rishi Satsangi <rishi.satsangi@gmail.com>
2016-01-28 09:27:21 -0500

    tail the logs

---
+++ logging_service/config.py
@@ -0,0 +1,31 @@
+import logging
+import os
+
+environment = os.environ.get('Environment', 'dev')
+
+output_file = os.environ.get(
+    'LOGGING_SERVICE_OUTPUT_FILE', '/var/log/logging-service.log')
+
+app_name = os.environ.get(
+    'LOGGING_SERVICE_APP_NAME', 'logging-service')
+
+logging_service_name = os.environ.get(
+    'LOGGING_SERVICE_LOGGER_NAME', 'logging-service')
+
+host = os.environ.get(
+    'LOGGING_SERVICE_HOST', '127.0.0.1')
+port = os.environ.get(
+    'LOGGING_SERVICE_PORT', '5000')
+
+logging_levels = {
+    "NOTSET": logging.NOTSET,
+    "DEBUG": logging.DEBUG,
+    "INFO": logging.INFO,
+    "WARNING": logging.WARNING,
+    "ERROR": logging.ERROR,
+    "CRITICAL": logging.CRITICAL
+}
+
+loggly_uri = os.environ.get(
+    'LOGGING_SERVICE_LOGGLY_URI',
+    'https://logs-01.loggly.com/inputs/77c54e3f-33b7-4f8b-a2a7-dbaef3414348/tag/python')
--- service.py
+++ service.py
@@ -1,9 +1,104 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import request
+import logging
+import loggly.handlers
+import json
+import time
+import subprocess
+from logging_service import config
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# configure logger
+logger = logging.getLogger(config.logging_service_name)
+logger.setLevel(logging.DEBUG)
+
+# stream handler
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+# file handler
+ch = logging.FileHandler(config.output_file)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+# loggly handler
+ch = loggly.handlers.HTTPSHandler(config.loggly_uri, 'POST')
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+import pdb; pdb.set_trace()
+
+
+# segment handler
+
+def _format(level, data):
+    """Converts an HTTP POST request to a log message
+
+    Args:
+        data: (JSON): JSON data to be formatted
+
+    Returns:
+        str: newly formatted log line with additional info.
+    """
+    # import pdb; pdb.set_trace()
+    data['time'] = time.time()
+    data['loggerName'] = logger.name
+    data['appName'] = app.name
+    return json.dumps(data, sort_keys=True)
+
+
+#create app
+app = Flask(config.app_name)
+
+@app.route('/syslog', methods=['POST'])
+def syslog():
+    """Log as system log
+
+    """
+    level = logging.DEBUG
+    if request.args['level'] != None:
+        level = config.logging_levels[request.args['level']]
+
+    message = _format(level, request.json)
+    logger.log(level, message)
+    return message
+
+@app.route('/pageview', methods=['POST'])
+def syslog():
+    """Log as page view
+
+    """
+    level = logging.INFO
+    message = _format(level, request.json)
+    logger.log(level, message)
+    return message
+
+@app.route('/event', methods=['POST'])
+def syslog():
+    """Log as clientside event
+
+    """
+    level = logging.INFO
+    message = _format(level, request.json)
+    logger.log(level, message)
+    return message
+
+
+@app.route('/tail', methods=['GET'])
+def tail():
+    num = 100
+    if request.args['n'] != None:
+        num = request.args['n']
+
+    proc = subprocess.Popen(["tail", "-n{num}".format(num=num), config.output_file], stdout=subprocess.PIPE)
+    output = proc.stdout.read()
+    return output
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=config.host, port=config.port)
